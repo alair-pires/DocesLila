1dfd2fea8a56a7710804c777be736b7f4b83ce42
alair-pires <alair-pires@outlook.com>
2023-07-30 13:09:04 -0300

    Inicio das migrations, instalando dependências do entity, aprendendo sobre criar tabelas e bancos

---
+++ DocesLila/Context/DocesLilaContext.cs
@@ -0,0 +1,12 @@
+﻿using DocesLila.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DocesLila.Context
+{
+    public class DocesLilaContext : DbContext
+    {
+        public DocesLilaContext(DbContextOptions<DocesLilaContext> options) : base(options)
+        {
+        }
+    }
+}
--- DocesLila/Startup.cs
+++ DocesLila/Startup.cs
@@ -1,13 +1,10 @@
+using DocesLila.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DocesLila
 {
@@ -24,6 +21,7 @@ namespace DocesLila
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<DocesLilaContext>(op => op.UseSqlServer(Configuration["ConnectionStrings:DefaultConnection"]));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
